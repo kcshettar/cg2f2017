84e1937abeec15f701dc6cfdc9fad962fe141de1
Kiran Shettar <kcshettar@gmail.com>
2017-11-12 11:15:43 -0500

    Update (Project Source Code)

---
+++ Project/Unity/HoloLens/Assets/CubeColor.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeColor : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        var newColor = new Color(Random.Range((float)0.0, (float)1.0), Random.Range((float)0.0, (float)1.0), Random.Range((float)0.0, (float)1.0));
+        this.GetComponent<Renderer>().material.color = newColor;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
